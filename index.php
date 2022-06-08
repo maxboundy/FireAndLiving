32d7cb6e1a537f12af897a525fe44ae0df0da7c0
maxboundy <78894664+maxboundy@users.noreply.github.com>
2022-06-08 12:13:17 +1200

    installed bootstrap to make website responsive

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Fire and Living </title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet"
+    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-
+awesome.min.css"> 
+
+    </head>
+</html> 
